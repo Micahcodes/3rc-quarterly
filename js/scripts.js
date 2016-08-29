48b9b3934b5af5f5ca77048e20b70ce9a8f298f8
Micah Stewart <stewart.micah@gmail.com>
2016-08-29 00:17:14 -0500

    add nav logic. Add news section. Add Work section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,45 @@
 $(document).ready(function() {
+		const navScrolled = "main-nav-scrolled";
+		const headerHeight = $('header').height();
 
-	/*============================================
-	Navigation Functions
-	==============================================*/
-	$(window).scroll(function(){
-		$(window).scrollTop() === 0 ?
-		$('nav').removeClass('scrolled') : $('nav').addClass('scrolled')
+		// navigation
+    $(window).scroll(function() {
+				const position = $(this).scrollTop();
+        if (position > headerHeight) {
+						$('nav').addClass('scrolled');
+				} else {
+						$('nav').removeClass('scrolled');
+				}
 
-		// highlight applicable menu item on page scroll
-	    // get the current window position and all menu section positions
-	    position = $(this).scrollTop();
-	    $('nav li a').each(function(){
-	        var anchorId = $(this).attr('href');
-	        var target = ($(anchorId).offset().top) - 150;
-	        
-	        // check if the current position has crossed a section
-	        if(position >= target) {
-	            $('nav li').removeClass("active")
-	            $(this).parent().addClass('active'); 
-	        }
-	    })
-	});
+        // highlight applicable menu item on page scroll
+        // get the current window position and all menu section positions
+        $('nav li a').each(function() {
+            const anchorId = $(this).attr('href');
+            const target = ($(anchorId).offset().top) - 150;
+
+            // check if the current position has crossed a section
+            if (position >= target) {
+                $('nav li').removeClass("active")
+                $(this).parent().addClass('active');
+            }
+        })
+    });
+
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > headerHeight) {
+    //         $('nav').addClass("scrolled");
+    //     } else {
+    //         $('nav').removeClass(scrolled);
+    //     }
+    // });
 
     // force scroll state after DOM ready
-	$(function(){
-	    $(window).scroll();
-	})
+    $(function() {
+        $(window).scroll();
+    })
 
-	// set menu scrolling to smooth scroll
-	$("nav a").smoothScroll({offset: -100});
+    // set menu scrolling to smooth scroll
+    $("nav a").smoothScroll({
+        offset: -100
+    });
 });
-
